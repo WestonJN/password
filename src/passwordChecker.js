198dc70430c78a08e32db68b8558389cd4035dc2
WestonJN <westonnyirenda@gmail.com>
2020-01-15 10:28:49 +0200

    Function edits

--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,12 +1,12 @@
 function passwordIsValid(password){
     try{
         if (password == '') throw "Password cannot be empty";
+        if (password.length <= 8) throw "Password should have more than 8 characters";
         if (password.match(/[a-z]/) == null) throw "Password should contain at least on lowercase letter";
         if (password.match(/[A-Z]/) == null) throw "Password should contain at least on uppercase letter";
         if (password.match(/[0-9]/) == null) throw "Password should contain at least one digit";
         if (password.match(/[!@#$%^&*()~<>?]/)== null)throw "Password should contain at least one character";
-        if (password.length <= 8) throw "Password should have more than 8 characters";
-    
+        
     }
     catch(error){
         console.log(error);
